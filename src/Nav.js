66bab5675860f4db5bbea01ec2329b959849eb87
Benjamin Lakey <benjaminlakey@outlook.com>
2022-08-08 00:20:15 +0100

    body padding+margin removed, nav built with a bit of style

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <nav className="Nav">
+      <img src="" alt="Logo" />
+      <div id="navOptions">
+        <a href="/">Home</a>
+        <a href="about">About Us</a>
+        <a href="recruitment">Recruitment</a>
+        <a href="contact">Contact Us</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
